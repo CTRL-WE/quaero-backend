f03ab17cccc02422a62553bc5c03c453b0d08b2a
deepakvishwa26 <deepakvishwa2625@gmail.com>
2026-07-25 10:14:17 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/ctrlwe/quaero/QuaeroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ctrlwe.quaero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuaeroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuaeroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctrlwe/quaero/QuaeroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctrlwe.quaero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuaeroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
